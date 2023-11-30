c282e35db2f9d2899ea6b54b0f6ba2ac7275d030
Irfan Rasheed <irfanrasheedadv@gmail.com>
2023-11-30 07:11:29 +0500

    integrate_vercel

---
+++ components/StartingPage/StartingPageContent.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './StartingPageContent.module.css';
+
+
+const StartingPageContent = () => {
+  return (
+    <section className={classes.starting}>
+      <h2>Welcome to IR_Attendance System!</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, aspernatur alias, magni dolore ea molestias praesentium beatae cumque
+        repellat assumenda maxime dicta totam hic doloribus, ut ipsum porro ipsam? Sapiente.</p>
+    </section>
+  );
+};
+
+export default StartingPageContent;
